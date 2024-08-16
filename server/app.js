57a37254dc11ee3224a2ad1f348dda987b74c79f
reborn_star1 <rebornstar677@gmail.com>
2024-08-16 19:28:30 +0530

    Used UseEffect React hook and socket emit and broadcast functions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,20 @@
 import express from 'express'
+import { Server } from 'socket.io';
+import { createServer } from "http";
+import cors from 'cors'
 
 const app = express();
+const server = new createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods: ["GET","POST"],
+        credentials: true,
+    }
+});
+
+app.use(cors());
 
 const PORT  = 3000;
 
@@ -9,6 +23,12 @@ app.get("/",(req,res) =>
     res.send("Hello World");
 })
 
-app.listen(PORT, () => {
+io.on("connection",(socket)=>{
+    console.log("User Connected");
+    console.log("ID",socket.id);
+    socket.broadcast.emit("welcome",`Welcome to the server, ${socket.id}`);
+})
+
+server.listen(PORT, () => {
     console.log(`The Server is Running on the Port ${PORT}`)
 });
